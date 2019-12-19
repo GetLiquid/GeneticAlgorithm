85d0a5cc5d880abae6124c826d78a14ac469b91b
Will Farris <9190155+WillFarris@users.noreply.github.com>
2019-12-18 21:57:25 -0800

    finished gene crossover

--- main.c
+++ main.c
@@ -22,33 +22,47 @@ int main()
         population[i] = new_critter();
         print_critter(population[i]);
     }
-
-    /* 2. Crossover the genomes of each critter with each other*/
-
-    printf("offspring (size %d):\n", POPULATION_SIZE * POPULATION_SIZE);
-
-    //array of critters of size POPULATION_SIZE^2 to accomodate all possible offspring
-    struct critter **generation = malloc(sizeof(struct critter *) * POPULATION_SIZE * POPULATION_SIZE);
     
-    for(int i=0;i<POPULATION_SIZE;++i)
+    do
     {
-    	for (int j = 0; j < POPULATION_SIZE; ++j)
-    	{
-    		//breed population[i] with population[j] and store in generation
-    		generation[j * POPULATION_SIZE + i] = crossover(population[i], population[j]);
-    		print_critter(generation[i]);
-    	}
-    }
+      /* 2. Crossover the genomes of each critter with each other*/
 
-    /* 3. Sort the offspring by fitness and choose the POPULATION_SIZE best genomes to be bred again */
+      //array of critters of size POPULATION_SIZE^2 to accomodate all possible offspring
+      struct critter **generation = malloc(sizeof(struct critter *) * POPULATION_SIZE * POPULATION_SIZE);
+      
+      for(int i=0;i<POPULATION_SIZE;++i)
+      {
+        for (int j = 0; j < POPULATION_SIZE; ++j)
+        {
+          //breed population[i] with population[j] and store in generation
+          generation[i * POPULATION_SIZE + j] = crossover(population[i], population[j]);
+          //print_critter(generation[i*POPULATION_SIZE+j]);
+        }
+      }
 
-	merge_sort(generation, 0, POPULATION_SIZE - 1, POPULATION_SIZE);
-    printf("best in generation (size %d): \n", POPULATION_SIZE * POPULATION_SIZE);
-    for(int i=0; i < (POPULATION_SIZE * POPULATION_SIZE) ;++i)
-    {;
-        print_critter(generation[i]);
-    }
+      /* 3. Sort the offspring by fitness to choose the POPULATION_SIZE best genomes to be bred again */
 
+      merge_sort(generation, 0, POPULATION_SIZE * POPULATION_SIZE - 1, POPULATION_SIZE * POPULATION_SIZE);
+      printf("best in generation (size %d): \n", POPULATION_SIZE * POPULATION_SIZE);
+      for(int i=0; i < POPULATION_SIZE;++i)
+      {
+          print_critter(generation[i]);
+      }
+
+
+      /* 4. The fittest offspring become the parents of the next generation */
+
+      for(int i=0;i<POPULATION_SIZE;++i)
+      {
+        free(population[i]);
+        population[i] = generation[i];
+      }
+      for(int i=POPULATION_SIZE;i<POPULATION_SIZE * POPULATION_SIZE;++i)
+      {
+        free(generation[i]);
+      }
+
+    } while(population[0]->fitness < INFINITY);
 
 
     return 0;
